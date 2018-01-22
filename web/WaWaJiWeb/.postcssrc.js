8e0ef85de43d0ebd8c17fcc1e4f5714b8728c0e7
QingShanLuoYue <980575067@qq.com>
2018-01-22 17:00:44 +0800

    添加postcss配置文件

---
+++ web/WaWaJiWeb/.postcssrc.js
@@ -0,0 +1,11 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+  "plugins": {
+    // to edit target browsers: use "browserlist" field in package.json
+    "autoprefixer": {
+    	// browsers: ["IE > 7", ">0.000000001% in CN"]
+    	browsers: ["Firefox >= 10", "IE >= 8", "chrome  >= 10", "safari  >= 10"]
+    }
+  }
+};
